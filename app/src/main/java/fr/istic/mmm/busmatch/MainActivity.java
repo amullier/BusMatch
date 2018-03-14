22db3ba3e1525a528373a71a22d922c26b83f645
Unknown <demeautis.victor@gmail.com>
2018-03-14 14:54:24 +0100

    ajout de trois fragments pour le menu

--- app/src/main/java/fr/istic/mmm/busmatch/MainActivity.java
+++ app/src/main/java/fr/istic/mmm/busmatch/MainActivity.java
@@ -29,6 +29,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
+import fr.istic.mmm.busmatch.fragment.DiscutionFragment;
+import fr.istic.mmm.busmatch.fragment.IsMatchFragment;
+import fr.istic.mmm.busmatch.fragment.ToMatchFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
@@ -73,10 +77,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         Fragment selectedFragment = null;
-                        switch (item.getItemId()) {
-                            case (R.id.tomatch):
-                            case (R.id.ismatch):
-                            case (R.id.discution):
+                        switch (item.getItemId()){
+                            case (R.id.tomatch) :
+                                selectedFragment = new ToMatchFragment();
+                                break;
+                            case (R.id.ismatch) :
+                                selectedFragment = new IsMatchFragment();
+                                break;
+                            case (R.id.discution) :
+                                selectedFragment = new DiscutionFragment();
+                                break;
                         }
                         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                         transaction.replace(R.id.frame_layout, selectedFragment);
@@ -87,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Manually displaying the first fragment - one time only
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        //transaction.replace(R.id.frame_layout, //select first fragment);
+        transaction.replace(R.id.frame_layout, new ToMatchFragment());
         transaction.commit();
     }
 
